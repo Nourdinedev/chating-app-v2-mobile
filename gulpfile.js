6051311363d6785c39b52302597b216a1d413be9
Nourdine Labib <Nour-Dine-XD@outlook.com>
2021-08-31 00:38:31 +0100

    add landing page and some styles

--- gulpfile.js
+++ gulpfile.js
@@ -12,19 +12,33 @@ const browsersync = require("browser-sync").create();
 // sass.compiler = require("dart-sass");
 
 // Sass Task
-function scssTask() {
-   return src("public/scss/style.scss", { sourcemaps: true }) // selecting the style/main scss file || { sourcemaps: true } will generate a .css.map file (show which scss file  the styles are coming from)
+function scssTask1() {
+   return src("public/scss/home/style.scss", { sourcemaps: true }) // selecting the style/main scss file || { sourcemaps: true } will generate a .css.map file (show which scss file  the styles are coming from)
       .pipe(sass()) // running sass function to compile scss to css
       .pipe(postcss([autoprefixer(), cssnano()])) // using autoprefixer function will add the browser prefixes to support older browsers || and cssnano function to minify css file
-      .pipe(dest("dist", { sourcemaps: "." })); // seting the destination of the compiled css and the map file to a folder called dist
+      .pipe(dest("dist/home", { sourcemaps: "." })); // seting the destination of the compiled css and the map file to a folder called dist
+}
+
+function scssTask2() {
+   return src("public/scss/register/style.scss", { sourcemaps: true }) // selecting the style/main scss file || { sourcemaps: true } will generate a .css.map file (show which scss file  the styles are coming from)
+      .pipe(sass()) // running sass function to compile scss to css
+      .pipe(postcss([autoprefixer(), cssnano()])) // using autoprefixer function will add the browser prefixes to support older browsers || and cssnano function to minify css file
+      .pipe(dest("dist/register", { sourcemaps: "." })); // seting the destination of the compiled css and the map file to a folder called dist
 }
 
 // JavaScript Task
-function jsTask() {
-   return src("public/js/script.js", { sourcemaps: true }) // selecting the js file || { sourcemaps: true } will generate a .js.map file (show which js file  the scripts are coming from)
+function jsTask1() {
+   return src("public/js/home/script.js", { sourcemaps: true }) // selecting the js file || { sourcemaps: true } will generate a .js.map file (show which js file  the scripts are coming from)
       .pipe(babel({ presets: ["@babel/preset-env"] })) // running babel function to compile JavaScript to older versions so old browsers can run it with no problems
       .pipe(terser()) // using terser function to minify JavaScript file
-      .pipe(dest("dist", { sourcemaps: "." })); // seting the destination of the compiled JavaScript and the map file to a folder called dist
+      .pipe(dest("dist/home", { sourcemaps: "." })); // seting the destination of the compiled JavaScript and the map file to a folder called dist
+}
+
+function jsTask2() {
+   return src("public/js/register/script.js", { sourcemaps: true }) // selecting the js file || { sourcemaps: true } will generate a .js.map file (show which js file  the scripts are coming from)
+      .pipe(babel({ presets: ["@babel/preset-env"] })) // running babel function to compile JavaScript to older versions so old browsers can run it with no problems
+      .pipe(terser()) // using terser function to minify JavaScript file
+      .pipe(dest("dist/register", { sourcemaps: "." })); // seting the destination of the compiled JavaScript and the map file to a folder called dist
 }
 
 // Browsersync
@@ -49,12 +63,12 @@ function browserSyncReload(cb) {
 
 // Watch Task
 function watchTask() {
-   watch("*.html", browserSyncReload);
+   watch("views/*.html", browserSyncReload);
    watch(
-      ["app/scss/**/*.scss", "app/**/*.js"],
-      series(scssTask, jsTask, browserSyncReload)
+      ["public/scss/**/**/*.scss", "bublic/**/*.js"],
+      series(scssTask1, scssTask2, jsTask1, jsTask2, browserSyncReload)
    );
 }
 
 // Default Gulp Task
-exports.default = series(scssTask, jsTask, watchTask);
+exports.default = series(scssTask1, scssTask2, jsTask1, jsTask2, watchTask);
